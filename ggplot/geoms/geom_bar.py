13040a374644f5a8aae498010e4b5ea203765621
ericchiang <eric.chiang.m@gmail.com>
2014-06-11 18:37:16 -0400

    multi bar plots added

--- ggplot/geoms/geom_bar.py
+++ ggplot/geoms/geom_bar.py
@@ -58,35 +58,94 @@ class geom_bar(geom):
         heights = pinfo.pop('y')
         labels = x
 
-        # layout and spacing
-        #
-        # matplotlib needs the left of each bin and it's width
-        # if x has numeric values then:
-        #   - left = x - width/2
-        # otherwise x is categorical:
-        #   - left = cummulative width of previous bins starting
-        #            at zero for the first bin
-        #
-        # then add a uniform gap between each bin
-        #   - the gap is a fraction of the width of the first bin
-        #     and only applies when x is categorical
-        _left_gap = 0
-        _spacing_factor = 0     # of the bin width
-        if not categorical:
-            left = np.array([x[i]-width[i]/2 for i in range(len(x))])
-        else:
+        unique_labels = np.unique(labels)
+        colors = pinfo['color']
+        unique_colors = np.unique(colors)
+
+
+        # Does this plot require stacking or dodging?
+        multi_plot = not len(unique_labels) == len(labels)
+        if(multi_plot):
+            position = self.params["position"]
+            # numpy array used for indexing
+            labels = np.array(x)
+            colors = np.array(pinfo.pop('color'))
+            heights = np.array(heights)
             _left_gap = 0.2
-            _spacing_factor = 0.105     # of the bin width
-            _breaks = np.append([0], width)
+            _spacing_factor = 0.105
+            # width assumed to be constant
+            width = width[:len(unique_labels)]
+            _breaks = np.append([0],width)
             left = np.cumsum(_breaks[:-1])
 
-        _sep = width[0] * _spacing_factor
-        left = left + _left_gap + [_sep * i for i in range(len(left))]
+            _sep = width[0] * _spacing_factor
+            left = left + _left_gap + [_sep * i for i in range(len(left))]
 
+            # stacked bar plots require keeping tack of the bottom of each row
+            _bottom = pd.Series(np.zeros(len(unique_labels)),
+                                index=unique_labels)
+            # dodged require calculating the fraction of width for each bar
+            _width_step = np.array(width / len(unique_colors)) 
+            for i,color in enumerate(unique_colors):
+                # use pandas indexing to ensure that zero values for labels
+                # along a particular fill are maintained as '0'
+                row_index = colors == color
+                row_heights = pd.Series(np.zeros(len(unique_labels)),
+                                        index=unique_labels)
+                row_heights.ix[labels[row_index]] = heights[row_index]
+                # determine type of multi-bar plot
+                if position == "stack":
+                    ax.bar(left=left,height=row_heights.values,
+                            width=width,bottom=_bottom.values,
+                            color=color)
+                    _bottom += row_heights
+                elif position == "dodge":
+                    _left = np.array(left) + (i * _width_step)
+                    ax.bar(left=_left,height=row_heights.values,
+                           width=_width_step,color=color)
+                else:
+                    raise Exception(
+                     "Only accepted 'position' parameters are " +\
+                     "'stack' and 'dodge'"
+                    )
 
-        ax.bar(left, heights, width, **pinfo)
-        ax.autoscale()
-
-        if categorical:
+            ax.autoscale()
             ax.set_xticks(left+width/2)
-            ax.set_xticklabels(x)
+            ax.set_xticklabels(unique_labels)
+
+        else:
+            # layout and spacing
+            #
+            # matplotlib needs the left of each bin and it's width
+            # if x has numeric values then:
+            #   - left = x - width/2
+            # otherwise x is categorical:
+            #   - left = cummulative width of previous bins starting
+            #            at zero for the first bin
+            #
+            # then add a uniform gap between each bin
+            #   - the gap is a fraction of the width of the first bin
+            #     and only applies when x is categorical
+            _left_gap = 0
+            _spacing_factor = 0     # of the bin width
+            if not categorical:
+                left = np.array([x[i]-width[i]/2 for i in range(len(x))])
+            else:
+                _left_gap = 0.2
+                _spacing_factor = 0.105     # of the bin width
+                _breaks = np.append([0], width)
+                left = np.cumsum(_breaks[:-1])
+
+            _sep = width[0] * _spacing_factor
+            left = left + _left_gap + [_sep * i for i in range(len(left))]
+
+
+            ax.bar(left, heights, width, **pinfo)
+            ax.autoscale()
+
+
+            if categorical:
+                ax.set_xticks(left+width/2)
+                ax.set_xticklabels(x)
+        # Always add "count"
+        ax.set_ylabel("count")
