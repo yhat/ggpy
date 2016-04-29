f7d3dba20124425f44058b47cce8702d208a7885
glamp <lamp.greg@gmail.com>
2016-04-28 22:24:02 -0400

    packging

---
+++ ggplot/__init__.py
@@ -0,0 +1,27 @@
+from geoms import geom_area, geom_line, geom_point, geom_histogram, geom_density, geom_hline, geom_vline, geom_bar, geom_abline
+from geoms import stat_smooth, stat_density
+
+from facets import facet_wrap, facet_grid
+
+from chart_components import ggtitle, xlim, ylim, xlab, ylab, labs
+
+from ggplot import ggplot
+
+from themes import theme_538, theme_gray, theme_xkcd
+
+from scales.scale_color_brewer import scale_color_brewer
+from scales.scale_color_manual import scale_color_manual
+from scales.scale_color_gradient import scale_color_gradient
+from scales.scale_log import scale_x_log, scale_y_log
+from scales.scale_reverse import scale_x_reverse, scale_y_reverse
+from scales.scale_x_continuous import scale_x_continuous
+from scales.scale_y_continuous import scale_y_continuous
+from scales.scale_x_discrete import scale_x_discrete
+from scales.scale_y_discrete import scale_y_discrete
+from scales.scale_x_date import scale_x_date
+from scales.scale_y_date import scale_y_date
+from scales.date_utils import date_format, date_breaks
+
+from coords.coords import coord_polar, coord_equal, coord_flip
+
+from aes import aes
