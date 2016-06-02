0ad81a6f93e684a9b83bd502793e7efe7031add7
glamp <lamp.greg@gmail.com>
2016-06-02 14:39:49 -0400

    docs

---
+++ generate-examples-html.py
@@ -0,0 +1,43 @@
+from sh import find
+import json
+import os
+
+rows = []
+for f in find("docs/examples"):
+    f = f.strip()
+    if f.endswith('.ipynb')==False or ".ipynb_checkpoints" in f:
+        continue
+    ipynb = json.load(open(f, 'rb'))
+
+
+    html = """
+<div class="col-sm-3">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title text-center">
+          <a href="{url}">{title}</a>
+        </h3>
+      </div>
+      <div class="panel-body">
+        <a href="{url}">
+          <img src="{img}" style="height: 200px; width: 100%" />
+        </a>
+      </div>
+    </div>
+</div>
+    """
+
+    for cell in ipynb['cells']:
+        if cell["outputs"]:
+            outputs = cell["outputs"]
+            for output in outputs:
+                if "image/png" in output['data']:
+                    png = output['data']['image/png'].replace('\n', '')
+                    url = "docs/examples/" + os.path.basename(f)
+                    title = os.path.basename(f)[:-6]
+                    img = "data:image/png;base64,%s" % png
+                    rows.append(html.format(title=title, img=img, url=url))
+                    break
+
+layout = open("template.html", "rb").read()
+print layout.format(body="\n".join(rows))
