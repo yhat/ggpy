4686011ef799432c4abb842573a0b9dbb17dc2c5
Greg Lamp <lamp.greg@gmail.com>
2016-07-22 15:55:10 -0400

    initial geom_polygon support

--- ggplot/geoms/geom_polygon.py
+++ ggplot/geoms/geom_polygon.py
@@ -2,11 +2,11 @@ from .geom import geom
 import matplotlib.patches as patches
 
 
-class geom_rect(geom):
+class geom_polygon(geom):
 
     DEFAULT_AES = {'alpha': None, 'color': None, 'fill': '#333333',
                    'linetype': 'solid', 'size': 1.0}
-    REQUIRED_AES = {'xmin', 'xmax', 'ymin', 'ymax'}
+    REQUIRED_AES = {'x', 'y'}
     DEFAULT_PARAMS = {}
     _aes_renames = {'linetype': 'linestyle', 'size': 'linewidth',
                     'fill': 'facecolor', 'color': 'edgecolor'}
@@ -18,8 +18,14 @@ class geom_rect(geom):
         y = data[variables['y']]
 
         coordinates = zip(x, y)
-
-        ax.add_patch(patches.Polygon(coordinates, closed=True, fill=False))   # Irregular Polygon
+        ax.add_patch(
+            patches.Polygon(
+                coordinates,
+                closed=True,
+                fill=True,
+                **params
+            )
+        )
         # matplotlib patches don't automatically impact the scale of the ax, so
         # we manually autoscale the x and y axes
         ax.autoscale_view()
---
+++ tests/test_geom_polygon.py
@@ -0,0 +1,14 @@
+from ggplot import *
+import pandas as pd
+
+
+
+df = pd.DataFrame({
+    "x": [0, 1, 1, 0] + [5, 10, 10, 5],
+    "y": [0, 0, 1, 1] + [10, 10, 20, 20],
+    "g": ["a", "a", "a", "a"] + ["b", "b", "b", "b"]
+})
+
+print ggplot(df, aes(x='x', y='y', fill='g')) + geom_polygon()
+print ggplot(df, aes(x='x', y='y', color='g')) + geom_polygon()
+print ggplot(df[df.g=="b"], aes(x='x', y='y')) + geom_polygon(alpha=0.25, linetype='dashed')
