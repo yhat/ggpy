436a4028f3bf805dc49be50d998d047966d377e9
Greg Lamp <lamp.greg@gmail.com>
2016-07-28 09:00:24 -0400

    whoops

--- ggplot/geoms/geom_rect.py
+++ ggplot/geoms/geom_rect.py
@@ -14,12 +14,20 @@ class geom_rect(geom):
     def plot(self, ax, data, _aes):
         params = self._get_plot_args(data, _aes)
         variables = _aes.data
-        x = data[variables['x']]
-        y = data[variables['y']]
+        xmin = data[variables['xmin']]
+        xmax = data[variables['xmax']]
+        ymin = data[variables['ymin']]
+        ymax = data[variables['ymax']]
 
-        coordinates = zip(x, y)
-
-        ax.add_patch(patches.Polygon(coordinates, closed=True, fill=False))   # Irregular Polygon
+        for (xmin_i, xmax_i, ymin_i, ymax_i) in zip(xmin.values, xmax.values, ymin.values, ymax.values):
+            ax.add_patch(
+                    patches.Rectangle(
+                        (xmin_i, ymin_i),       # (x,y)
+                        xmax_i - xmin_i ,          # width
+                        ymax_i - ymin_i ,          # height
+                        **params
+                    )
+            )
         # matplotlib patches don't automatically impact the scale of the ax, so
         # we manually autoscale the x and y axes
         ax.autoscale_view()
