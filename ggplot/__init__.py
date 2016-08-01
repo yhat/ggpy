63c696b23f879e5df2e827f27c53504c769d397e
Greg Lamp <lamp.greg@gmail.com>
2016-08-01 10:42:27 -0400

    bump

--- ggplot/__init__.py
+++ ggplot/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
-__version__ = '0.10.10'
+__version__ = '0.11.0'
 
 # For testing purposes we might need to set mpl backend before any
 # other import of matplotlib.
