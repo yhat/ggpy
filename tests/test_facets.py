b9734a3034ee2b1de79fcb700006d7aa3252e3c9
Greg Lamp <lamp.greg@gmail.com>
2016-08-01 20:45:14 -0400

    tests

--- tests/test_facets.py
+++ tests/test_facets.py
@@ -5,7 +5,7 @@ diaa = diamonds[['cut','color','table']]
 diab = diaa.groupby(['cut','color']).quantile([x/100.0 for x in range(0,100,5)])
 diab.reset_index(inplace=True)
 diab.columns = ['cut','color','p','stats']
-# print ggplot(diab,aes(x='p', weight='stats')) + geom_bar() + facet_grid('color','cut')
+print ggplot(diab,aes(x='p', weight='stats')) + geom_bar() + facet_grid('color','cut')
 
 print ggplot(diamonds, aes(x='clarity', weight='price')) + geom_bar() + facet_grid('color', 'cut')
 print ggplot(diamonds, aes(x='clarity', weight='price', fill='color')) + geom_bar() + facet_grid('color', 'cut')
